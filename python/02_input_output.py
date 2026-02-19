921eaeef6e7ab7551dfe7c461082b4926f2fa484
Ansh Mittal <anshmit657@gmail.com>
2026-02-19 16:00:00 +0530

    add input-output and variable

---
+++ python/02_input_output.py
@@ -0,0 +1,11 @@
+# print(): Print is built in function used to print text in the standard input and output screen.
+
+# print("Hello world")
+
+
+#input(): input is built in function used to take input from the user.
+# Note:- Input function alway gives you text
+
+username = input("Enter your name: ")
+print("Hello", username)
+print("Hello " + username)
---
+++ python/03_variable.py
@@ -0,0 +1,18 @@
+# Variable:- Variable is like a container which can store a value so that we can use later in the code.
+
+# name = input("Enter your name: ")
+# print("Your name is " + name)
+
+# Your name is ----- and you order ----.
+name = input("Enter your name: ")
+food = input("Enter your order: ")
+
+print("Your name is " + name + " and you order " + food + ".")
+print(f"Your name is {name} and you order {food}.")
+
+
+
+# Task
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# Your name is ---- and you are ---- year old.
