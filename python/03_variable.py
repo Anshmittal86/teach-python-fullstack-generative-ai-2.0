91b8b201403371096af46b6ac1a6a251135d8d8f
Ansh Mittal <anshmit657@gmail.com>
2026-02-19 16:38:25 +0530

    add data types

--- python/03_variable.py
+++ python/03_variable.py
@@ -1,7 +1,7 @@
 # Variable:- Variable is like a container which can store a value so that we can use later in the code.
 
-# name = input("Enter your name: ")
-# print("Your name is " + name)
+name = input("Enter your name: ")
+print("Your name is " + name)
 
 # Your name is ----- and you order ----.
 name = input("Enter your name: ")
@@ -10,9 +10,16 @@ food = input("Enter your order: ")
 print("Your name is " + name + " and you order " + food + ".")
 print(f"Your name is {name} and you order {food}.")
 
-
-
 # Task
 # name = input("Enter your name: ")
 # age = input("Enter your age: ")
-# Your name is ---- and you are ---- year old.
+# print(f"Your name is {name} and you are {age} year old.")
+
+# Multiple Assignment
+# a = 1
+# b = 2
+# c = 3
+a, b, c = 1, 2, 3
+
+# Same Value to multiple Variable
+a = b = c = 1
---
+++ python/04_data_types.py
@@ -0,0 +1,31 @@
+# Data type:- Type of the data which are stored in the variable.
+
+# number :- store numbers (10, 5.45 etc.)
+# total_score = 100
+# print(f"Your score is {total_score}.")
+
+# string:- stores text 
+# name = input("Enter your name: ")
+# print(name)
+
+# complex number
+# cnum = 3 + 4j
+# print(cnum)
+
+# Boolean :- True and False
+# is_student = True
+# print(is_student)
+
+
+# type check
+name = "Aman"
+print(name, type(name))
+
+age = 10
+print(age, type(age))
+
+is_login = False
+print(is_login, type(is_login))
+
+cnum = 4 + 6j
+print(cnum, type(cnum))
